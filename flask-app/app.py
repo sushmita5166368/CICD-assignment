be53b8025adde1e4878ed427320672d35cfa58a9
sushmita5166368 <sushmita5166368@gmail.com>
2026-08-13 22:17:13 +0530

    Update app.py

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,12 +4,12 @@ app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return "HEllo from Flask!"
+    return "Hello from Flask!"
 
 @app.route("/health")
 def health():
     return jsonify(status="Healthy"),
-200
+    200
 
 if __name__=="__main__":
     app.run(host ="0.0.0.0" , port=5000)
